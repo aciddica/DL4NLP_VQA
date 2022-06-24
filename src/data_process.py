f163762e363cf997883f9b3bd10bc1113c74ecfd
aciddica <76953433+aciddica@users.noreply.github.com>
2022-06-24 22:32:48 +0800

    test merge

--- src/data_process.py
+++ src/data_process.py
@@ -8,60 +8,57 @@ from PIL import Image
 from easydict import EasyDict as edict
 
 class VQSdataset:
-    def __init__(self, mode = "train", q_dict = None, a_dict = None):
-        self.imgdir_path = ""
-        self.annotations = {}
-        self.qusetion = {}
+    def __init__(self, images, mode = "train", q_dict = None, a_dict = None):
+        # self.imgdir_path = ""
+        # self.annotations = {}
+        # self.qusetion = {}
         self.q_dict = q_dict
         self.a_dict = a_dict
-        self.Resize = vision.Resize((512, 512))
-        self.CenterCrop =  vision.CenterCrop(448)
-        self.images = []
-        print (os.getcwd())
-        if mode == "train":
-            self.imgdir_path = "./data/images/train"
-            self.annotations = json.load(open("./data/annotations/train.json"))
-            self.qusetion = json.load(open("./data/questions/train.json"))
-        elif mode == "test":
-            self.imgdir_path = "./data/images/test"
-            self.annotations = json.load(open("./data/annotations/test.json"))
-            self.qusetion = json.load(open("./data/questions/test.json"))
-        else:
-            self.imgdir_path = "./data/images/val"
-            self.annotations = json.load(open("./data/annotations/val.json"))
-            self.qusetion = json.load(open("./data/questions/val.json"))
-        img_count = 0
-        for index, item in enumerate(self.annotations['annotations']):
-            img_path = os.path.join(self.imgdir_path, "COCO_{0}2014_{1}.jpg".format(mode,str(item['image_id']).zfill(12))) 
-            if os.path.exists(img_path):
-                img_count += 1
-                continue
-            self.images.append(img_path)
+        # self.Resize = vision.Resize((512, 512))
+        # self.CenterCrop =  vision.CenterCrop(448)
+        self.images = images
+        # print (os.getcwd())
+        # if mode == "train":
+        #     self.imgdir_path = "./data/images/train"
+        #     self.annotations = json.load(open("./data/annotations/train.json"))
+        #     self.qusetion = json.load(open("./data/questions/train.json"))
+        # elif mode == "test":
+        #     self.imgdir_path = "./data/images/test"
+        #     self.annotations = json.load(open("./data/annotations/test.json"))
+        #     self.qusetion = json.load(open("./data/questions/test.json"))
+        # else:
+        #     self.imgdir_path = "./data/images/val"
+        #     self.annotations = json.load(open("./data/annotations/val.json"))
+        #     self.qusetion = json.load(open("./data/questions/val.json"))
+        # img_count = 0
+        # for index, item in enumerate(self.annotations['annotations']):
+        #     img_path = os.path.join(self.imgdir_path, "COCO_{0}2014_{1}.jpg".format(mode,str(item['image_id']).zfill(12))) 
+        #     if os.path.exists(img_path):
+        #         img_count += 1
+        #         continue
+        #     self.images.append(img_path)
         
     def __getitem__(self, index):
-        image = Image.open(self.images[index]).convert('RGB')
-        image = self.Resize(image)
-        image = self.CenterCrop(image)
-        return image
+        return self.images[index]
 
     def __len__(self):
         return 233
 
-def img2tensor(img):
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    transform = edict({
-        "ToPIL": vision.ToPIL(),
-        "Decode": vision.Decode(),
-        "Resize": vision.Resize((512, 512)),
-        "CenterCrop": vision.CenterCrop(448),
-        "ToTensor":vision.ToTensor(),
-        "Normalize": vision.Normalize(mean=mean, std=std),
-        "HWC2CHW": vision.HWC2CHW(),
-    })
-    img = vision.HWC2CHW(img) / 255
-    img = vision.Normalize(img)
-    return img
+# def img2tensor(img):
+#     mean = [0.485, 0.456, 0.406]
+#     std = [0.229, 0.224, 0.225]
+#     transform = edict({
+#         "ToPIL": vision.ToPIL(),
+#         "Decode": vision.Decode(),
+#         "Resize": vision.Resize((512, 512)),
+#         "CenterCrop": vision.CenterCrop(448),
+#         "ToTensor":vision.ToTensor(),
+#         "Normalize": vision.Normalize(mean=mean, std=std),
+#         "HWC2CHW": vision.HWC2CHW(),
+#     })
+#     img = transform.HWC2CHW(img) / 255
+#     img = transform.Normalize(img)
+#     return img
 
 class DistributedSampler():
     """
@@ -94,12 +91,13 @@ class DistributedSampler():
 
     def __len__(self):
         return self.num_samples
-def create_dataset(batch_size, mode = 'train',drop_remainder = True, q_dict = None, a_dict = None):
+        
+def create_dataset(batch_size, images, mode = 'train',drop_remainder = True, q_dict = None, a_dict = None):
     
-    raw_dataset = VQSdataset(mode)
+    raw_dataset = VQSdataset(mode, images)
     sampler = DistributedSampler(raw_dataset)
     dataset = ds.GeneratorDataset(raw_dataset, ["image", "question", "label"], shuffle = False, sampler = sampler)
-    dataset = dataset.map(operations = img2tensor, input_columns = "image", num_parallel_workers = 8)
+    # dataset = dataset.map(operations = img2tensor, input_columns = "image", num_parallel_workers = 8)
     dataset = dataset.batch(batch_size, drop_remainder = drop_remainder)
 
     print("creating dataset...")
---
+++ src/image_set.py
@@ -0,0 +1,67 @@
+'''
+因为val和test中图片有重复，考虑建一个大图片池，给不同数据集分别引用，如：
+class DataSet:
+    def __init__(self, images, ...):
+        self.images = images
+        ...
+images = ImageSet(128)
+dataset_train = DataSet(images, ...)
+dataset_val = DataSet(images, ...)
+...
+访问不存在的下标会返回零张量，这样没图的问题也可以正常参与训练了。
+'''
+import PIL.Image
+import json
+import numpy
+import os
+import re
+import mindspore
+path_data = 'data'
+class ImageSet:
+    @staticmethod
+    def process(image_size):
+        os.makedirs(f'image_set_{image_size}', exist_ok = True)
+        for part in 'train', 'val', 'test':
+            if part != 'test':
+                with open(f'{path_data}/questions/{part}.json') as f:
+                    image_id_set = set(i['image_id'] for i in json.load(f)['questions'])
+            for name in os.listdir(f'{path_data}/images/{part}'):
+                image_id = int(re.match(r'COCO_(train|val)2014_(\d+).jpg', name).group(2))
+                if (part == 'test') ^ (image_id in image_id_set):
+                    with PIL.Image.open(f'{path_data}/images/{part}/{name}') as image:
+                        h = image.height
+                        w = image.width
+                        m = max(h, w)
+                        image = image.crop(((w - m) // 2, (h - m) // 2, (w + m) // 2, (h + m) // 2))
+                        image = image.resize((image_size, image_size))
+                        image = image.convert('RGB')
+                        image = numpy.array(image)
+                        image = numpy.ascontiguousarray(image.transpose(2, 0, 1))
+                        with open(f'image_set_{image_size}/{image_id}', 'wb') as f:
+                            numpy.save(f, image)
+    def _load_image(self, index):
+        with open(f'image_set_{self.image_size}/{index}', 'rb') as f:
+            return numpy.load(f)
+    def __init__(self, image_size, dtype = mindspore.float64):
+        '''e.g.
+        images = ImageSet(64)
+        images[i] # returns mindspore.Tensor of shape 3, 64, 64
+        '''
+        if not os.path.exists(f'image_set_{image_size}'):
+            self.process(image_size)
+        self.image_size = image_size
+        self.dtype = dtype
+        self.in_memory = image_size < 256
+        self.empty = numpy.zeros((3, self.image_size, self.image_size))
+        self.image_list = [self.empty] * 600000
+        for i in os.listdir(f'image_set_{self.image_size}'):
+            self.image_list[int(i)] = self.in_memory and self._load_image(i)
+    def __getitem__(self, index):
+        '''e.g.
+        images[9] # returns mindspore.Tensor of shape 3, 64, 64
+        images[0] # returns zero tensor of the shape (no such image)
+        '''
+        image = self.image_list[index]
+        if image is False:
+            image = self._load_image(index)
+        return mindspore.Tensor(image, self.dtype)
