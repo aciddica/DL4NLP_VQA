69e5b33179c1e9ba7ca889348d8c39d7e840c92a
xxhzju <108112486+xxhzju@users.noreply.github.com>
2022-06-26 11:33:08 +0800

    Add files via upload
    new API to replace data_process.py

---
+++ src/vqa_set.py
@@ -0,0 +1,23 @@
+import mindspore.dataset
+from image_set import ImageSet
+from ... import QASet # filename?
+class VQAPart:
+    def __init__(self, images, qas):
+        self.images = images
+        self.qas = qas
+    def __getitem__(self, index):
+        image_id, question, answer = self.qas[index]
+        return self.images[image_id], question, answer
+class VQASet:
+    def _part(part):
+        part = VQAPart(self.images, QASet(part))
+        part = mindspore.dataset.GeneratorDataset(part, ['image', 'question', 'answer'])
+        return part
+    def __init__(self, size_batch):
+        self.images = ImageSet()
+        self.train = self._part('train')
+        self.train = self.train.batch(size_batch)
+        self.val = self._part('val')
+        self.val = self.val.batch(self.val.get_dataset_size())
+        self.test = self._part('test')
+        self.test = self.test.batch(self.test.get_dataset_size())
